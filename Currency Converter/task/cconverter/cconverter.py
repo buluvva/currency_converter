fa902b4e4d5609b6612a3f95cb7418b5dcd80ef4
maksimpiven <vietnam-11@mail.ru>
2021-11-02 16:51:43 +0300

    update

--- Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/task/cconverter/cconverter.py
@@ -6,17 +6,18 @@ cache = dict()
 url = f'http://www.floatrates.com/daily/'
 
 def convert(in_value):
-    
-        out_value = input()
-        amount = int(input())
-        r = requests.get(f'{url}{in_value}.json')
-        data = r.json()
-        if f'{in_value}' in cache.values():
-            print(cache[f'{out_value}']['rate'])
-        else:
-            print("cache fail")
-            #cache.update[f'{out_value}']['rate']
-            print(data[f'{out_value}']['rate'])
+
+    out_value = input()
+    amount = int(input())
+    r = requests.get(f'{url}{in_value}.json')
+    data = r.json()
+    if f'{in_value}' in cache.values():
+       print(int(cache[f'{out_value}']['rate'])*amount)
+    else:
+        print("cache fail")
+        #cache.update[f'{out_value}']['rate']
+        result = float(data[f'{out_value}']['rate'])*amount
+        print(f'{result} {out_value}')
 
 in_value = input()
 
